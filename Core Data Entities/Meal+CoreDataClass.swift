d910187e2ea69c5c75229d5b3f16e889ec5748e8
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-04-26 00:03:29 +0400

    Add: adding and deleting data

---
+++ Core Data Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  FoodTime
+//
+//  Created by Екатерина  on 25.04.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ Core Data Entities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  FoodTime
+//
+//  Created by Екатерина  on 25.04.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
